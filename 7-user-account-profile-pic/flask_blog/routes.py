1c3574895921ba83cf612b742264e581e84f5eec
Harshit <bajpaiharshit14@gmail.com>
2020-06-08 01:17:13 +0530

    user account and profile pic

---
+++ 7-user-account-profile-pic/flask_blog/routes.py
@@ -0,0 +1,109 @@
+from flask import render_template, url_for, flash, redirect, request
+from flask_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flask_blog.models import User, Post
+from flask_blog import app, db, bcrypt
+from flask_login import login_user, current_user, logout_user, login_required
+import secrets, os
+from PIL import Image
+
+post = [
+    {
+        'author': 'harshit bajpai',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2020'
+
+    },
+    {
+        'author': 'Jane doe',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2020'
+
+    }
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=post)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if(current_user.is_authenticated):
+        flash('already logged in')
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('account created, you are now able to login', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if(current_user.is_authenticated):
+        flash('already logged in')
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if(user and bcrypt.check_password_hash(user.password, form.password.data)):
+            flash(f'{form.email.data} is logged in!', 'success')
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            if next_page:
+                return redirect(next_page)
+            else:
+                return redirect(url_for('home'))
+        else:
+            flash('Login failed. Please check email and/or password', 'danger')
+
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash('logged out ')
+    return redirect(url_for('home'))
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('account updated', 'success')
+        return redirect(url_for('account'))
+    elif(request.method == 'GET'):
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/'+ current_user.image_file)
+
+    return render_template('account.html', title='Account', image_file=image_file, form = form)
+    
