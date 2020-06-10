db5ff46da9c9c3392c75d24c0749a3d6d862f270
Harshit <bajpaiharshit14@gmail.com>
2020-06-10 19:59:53 +0530

    updated config file

--- 12-Error-Pages/flask_blog/config.py
+++ 12-Error-Pages/flask_blog/config.py
@@ -1,9 +1,11 @@
 import os
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = os.environ.get('SECRET_KEY')  #need to be set in bashrc
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')  #need to be set in bashrc
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')  #need to be set in bashrc
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')  #need to be set in bashrc
+
+#after setting in bashrc, you might need to restart your terminal/system for bshrc to run again or you can run it again manually or you can export these variables in shell manually
